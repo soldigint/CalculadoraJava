f158f816b31bc2a0dbbad7751f862d250fd64867
Laura Patricia Ramirez Rivera <97982013+soldigint@users.noreply.github.com>
2022-08-11 12:48:43 -0500

    Create fondo.java

---
+++ fondo.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+import javax.swing.*;
+
+public class fondo extends JPanel{
+	
+	fondo(int lx, int ly, int tx, int ty)
+	{
+		setLocation(lx,ly);
+		setSize(tx,ty);
+		setLayout(null);
+		
+	}
+
+}
