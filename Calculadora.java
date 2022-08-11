4a5a408bc4311da82064496a4aafbed8dea8457e
Laura Patricia Ramirez Rivera <97982013+soldigint@users.noreply.github.com>
2022-08-11 12:47:53 -0500

    Create Calculadora.java

---
+++ Calculadora.java
@@ -0,0 +1,14 @@
+package calculadora;
+
+import java.awt.Color;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		ventana principal=new ventana(10,10,500,500);
+		
+		
+	}
+
+}
